ddd49e4b316d9b0a328beb3755f4d42680417bef
RSoraM <soramephisto@gmail.com>
2024-09-19 03:35:18 +0800

    test: 💍 rabbit case
    增加一些测试案例

--- src/cipher/rabbit.ts
+++ src/cipher/rabbit.ts
@@ -5,46 +5,6 @@ import { KitError, rotateL32 } from '../core/utils'
 
 const A = new Uint32Array([0x4D34D34D, 0xD34D34D3, 0x34D34D34, 0x4D34D34D, 0xD34D34D3, 0x34D34D34, 0x4D34D34D, 0xD34D34D3])
 
-// * Functions
-
-function setupKey(K: Uint8Array, X: Uint8Array, C: Uint8Array) {
-  const K16 = new Uint16Array(K.buffer)
-  const X32 = new Uint32Array(X.buffer)
-  const C32 = new Uint32Array(C.buffer)
-  for (let i = 0; i < 8; i++) {
-    if (i % 2 === 0) {
-      const KH = K16[(i + 1) % 8]
-      const KL = K16[i]
-      X32[i] = (KH << 16) | KL
-      const CH = K16[(i + 4) % 8]
-      const CL = K16[(i + 5) % 8]
-      C32[i] = (CH << 16) | CL
-    }
-    else {
-      const KH = K16[(i + 5) % 8]
-      const KL = K16[(i + 4) % 8]
-      X32[i] = (KH << 16) | KL
-      const CH = K16[i]
-      const CL = K16[(i + 1) % 8]
-      C32[i] = (CH << 16) | CL
-    }
-  }
-}
-
-function setupIV(iv: Uint8Array, C: Uint8Array) {
-  const iv32 = new Uint32Array(iv.buffer)
-  const iv16 = new Uint16Array(iv.buffer)
-  const C32 = new Uint32Array(C.buffer)
-  C32[0] ^= iv32[0]
-  C32[1] ^= (iv16[3] << 16) | iv16[1]
-  C32[2] ^= iv32[1]
-  C32[3] ^= (iv16[2] << 16) | iv16[0]
-  C32[4] ^= iv32[0]
-  C32[5] ^= (iv16[3] << 16) | iv16[1]
-  C32[6] ^= iv32[1]
-  C32[7] ^= (iv16[2] << 16) | iv16[0]
-}
-
 // * Rabbit Algorithm
 
 /**
@@ -73,7 +33,7 @@ export const rabbit = createIVStreamCipher(
       throw new KitError('rabbit requires a key of 16 bytes')
     }
 
-    // 初始化变量
+    // 内部状态
     let carray = 0
     const X = new Uint8Array(32)
     const C = new Uint8Array(32)
@@ -82,18 +42,18 @@ export const rabbit = createIVStreamCipher(
     const nextState = (skipExtract: boolean = false): Uint8Array => {
       // Counter System
       for (let i = 0; i < 8; i++) {
-        const _C = C32[i]
-        C32[i] = (C32[i] + A[i] + carray) | 0
-        carray = C32[i] < _C ? 1 : 0
+        const T = C32[i] + A[i] + carray
+        C32[i] = T | 0
+        carray = T > 0xFFFFFFFF ? 1 : 0
       }
 
       // G
       const G = new Uint32Array(8)
-      G.forEach((_, i) => {
-        const temp = (BigInt(X32[i]) + BigInt(C32[i])) & 0xFFFFFFFFn
-        const square = temp * temp
-        G[i] = Number((square ^ (square >> 32n)) & 0xFFFFFFFFn)
-      })
+      for (let i = 0; i < 8; i++) {
+        const T = (BigInt(X32[i]) + BigInt(C32[i])) & 0xFFFFFFFFn
+        const S = T * T
+        G[i] = Number((S ^ (S >> 32n)) & 0xFFFFFFFFn)
+      }
 
       // Next State
       X32[0] = 0xFFFFFFFF & (G[0] + rotateL32(G[7], 16) + rotateL32(G[6], 16))
@@ -118,47 +78,76 @@ export const rabbit = createIVStreamCipher(
       return new Uint8Array(S.buffer)
     }
 
-    // 配置密钥
-    setupKey(K, X, C)
-    for (let i = 0; i < 4; i++) {
-      nextState(true)
-    }
-    for (let i = 0; i < 8; i++) {
-      C32[i] ^= X32[(i + 4) % 8]
-    }
-
-    // 配置 IV
-    if (iv.byteLength === 8) {
-      setupIV(iv, C)
+    // 初始化
+    (() => {
+      // 配置密钥
+      const K16 = new Uint16Array(K.buffer)
+      for (let i = 0; i < 8; i++) {
+        if ((i & 1) === 0) {
+          const KH = K16[(i + 1) % 8]
+          const KL = K16[i]
+          X32[i] = (KH << 16) | KL
+          const CH = K16[(i + 4) % 8]
+          const CL = K16[(i + 5) % 8]
+          C32[i] = (CH << 16) | CL
+        }
+        else {
+          const KH = K16[(i + 5) % 8]
+          const KL = K16[(i + 4) % 8]
+          X32[i] = (KH << 16) | KL
+          const CH = K16[i]
+          const CL = K16[(i + 1) % 8]
+          C32[i] = (CH << 16) | CL
+        }
+      }
       for (let i = 0; i < 4; i++) {
         nextState(true)
       }
-    }
-    else if (iv.byteLength !== 0 && iv.byteLength !== 8) {
-      throw new KitError('rabbit requires a iv of 8 bytes')
-    }
+      for (let i = 0; i < 8; i++) {
+        C32[i] ^= X32[(i + 4) % 8]
+      }
+
+      // 配置 IV
+      if (iv.byteLength === 8) {
+        const iv32 = new Uint32Array(iv.buffer)
+        const iv16 = new Uint16Array(iv.buffer)
+        C32[0] ^= iv32[0]
+        C32[1] ^= (iv16[3] << 16) | iv16[1]
+        C32[2] ^= iv32[1]
+        C32[3] ^= (iv16[2] << 16) | iv16[0]
+        C32[4] ^= iv32[0]
+        C32[5] ^= (iv16[3] << 16) | iv16[1]
+        C32[6] ^= iv32[1]
+        C32[7] ^= (iv16[2] << 16) | iv16[0]
+        for (let i = 0; i < 4; i++) {
+          nextState(true)
+        }
+      }
+      else if (iv.byteLength !== 0 && iv.byteLength !== 8) {
+        throw new KitError('rabbit requires a iv of 8 bytes')
+      }
+    })()
 
     // 密钥流
-    let S = new Uint8Array(0)
-    let current = 0
+    let S = nextState()
+    let current = 1
     const squeeze = (count: number) => {
       if (current >= count) {
         return S
       }
-      const temp = new Uint8Array(count << 4)
-      temp.set(S, 0)
-      // console.log(temp.buffer)
+      const T = new Uint8Array(count << 4)
+      T.set(S, 0)
+      S = T
       while (current < count) {
-        temp.set(nextState(), current << 4)
+        S.set(nextState(), current << 4)
         current++
       }
-      S = temp
       return S
     }
     const cipher = (M: Uint8Array) => {
-      const BLOCK_TOTAL = Math.ceil(M.byteLength >> 4) + 1
+      const BLOCK_TOTAL = Math.ceil(M.byteLength >> 4) || 1
       S = squeeze(BLOCK_TOTAL)
-      return M.map((byte, i) => byte ^ S[i])
+      return M.map((_, i) => _ ^ S[i])
     }
 
     return {
