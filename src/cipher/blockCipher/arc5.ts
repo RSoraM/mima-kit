ddb54ef51ed138e8820d45b7d632f2d7dcf89966
RSoraM <soramephisto@gmail.com>
2024-12-17 14:13:15 +0800

    refactor: 💡 重构RC5，简化代码

--- src/cipher/blockCipher/arc5.ts
+++ src/cipher/blockCipher/arc5.ts
@@ -1,5 +1,5 @@
 import { createCipher } from '../../core/cipher'
-import { KitError, U8, resizeBuffer, rotateL128, rotateL16, rotateL32, rotateL64, rotateL8, rotateR128, rotateR16, rotateR32, rotateR64, rotateR8 } from '../../core/utils'
+import { KitError, U8, genBitMask, resizeBuffer, rotateL, rotateR } from '../../core/utils'
 
 // const Eul = [0xB7, 0xE1, 0x51, 0x62, 0x8A, 0xED, 0x2A, 0x6A, 0xBF, 0x71, 0x58, 0x80, 0x9C, 0xF4, 0xF3, 0xC7, 0x62, 0xE7, 0x16, 0x0F, 0x38, 0xB4, 0xDA, 0x56, 0xA7, 0x84, 0xD9, 0x04, 0x51, 0x90, 0xCF, 0xEF]
 // const Phi = [0x9E, 0x37, 0x79, 0xB9, 0x7F, 0x4A, 0x7C, 0x15, 0xF3, 0x9C, 0xC0, 0x60, 0x5C, 0xED, 0xC8, 0x34, 0x10, 0x82, 0x27, 0x6B, 0xF3, 0xA2, 0x72, 0x51, 0xF8, 0x6C, 0x6A, 0x11, 0xD0, 0xC1, 0x8E, 0x95]
@@ -8,373 +8,113 @@ import { KitError, U8, resizeBuffer, rotateL128, rotateL16, rotateL32, rotateL64
 
 // * Functions
 
-function setup8(K: Uint8Array, r: number) {
-  const P = 0xB7
-  const Q = 0x9F
-  // Break the key into 8-bit words
-  const L = K.byteLength === 0
-    ? new Uint8Array(1)
-    : K.slice(0)
-  const c = L.length
+function _setup(key: Uint8Array, word_size: number, round: number, mask: bigint) {
+  const word_bit = BigInt(word_size)
+  const word_byte = word_size >> 3
+  const P = (0xB7E151628AED2A6ABF7158809CF4F3C7n >> (128n - word_bit)) | 1n
+  const Q = (0x9E3779B97F4A7C15F39CC0605CEDC835n >> (128n - word_bit)) | 1n
+  // Break the key into w-bit words
+  const c = Math.ceil((key.length || 1) / word_byte)
+  const L = resizeBuffer(key, c * word_byte)
+  const LV = L.view(word_byte)
   // Initialize key-independent pseudorandom S array
-  const t = (r + 1) << 1
-  const S = new Uint8Array(t)
-  S[0] = P
+  const t = (round + 1) << 1
+  const S = new U8(t * word_byte)
+  const SV = S.view(word_byte)
+  // S[0] = P
+  let prv = P
+  SV.set(0, prv, true)
   for (let i = 1; i < t; i++) {
-    S[i] = S[i - 1] + Q
+    // S[i] = S[i-1] + Q
+    prv = (prv + Q) & mask
+    SV.set(i, prv, true)
   }
   // The main key scheduling loop
   let i = 0
   let j = 0
-  let A = 0
-  let B = 0
+  let A = 0n
+  let B = 0n
   const v = 3 * Math.max(c, t)
   for (let k = 0; k < v; k++) {
-    A = S[i] = rotateL8(S[i] + A + B, 3)
-    B = L[j] = rotateL8(L[j] + A + B, A + B)
+    // A = S[i] = (S[i] + A + B) <<< 3
+    const S = SV.get(i, true)
+    A = rotateL(word_bit, S + A + B, 3n, mask)
+    SV.set(i, A, true)
+    // B = L[j] = (L[j] + A + B) <<< (A + B)
+    const L = LV.get(j, true)
+    B = rotateL(word_bit, L + A + B, A + B, mask)
+    LV.set(j, B, true)
+    // i = (i + 1) mod t
     i = (i + 1) % t
+    // j = (j + 1) mod c
     j = (j + 1) % c
   }
   return S
 }
-function encrypt8(M: Uint8Array, S: Uint8Array, r: number) {
-  const C = M.slice(0)
-  C[0] += S[0]
-  C[1] += S[1]
-  for (let i = 1; i <= r; i++) {
-    C[0] = rotateL8(C[0] ^ C[1], C[1] % 8)
-    C[0] += S[(i << 1)]
-    C[1] = rotateL8(C[1] ^ C[0], C[0] % 8)
-    C[1] += S[(i << 1) + 1]
+function _encrypt(M: Uint8Array, S: Uint8Array, word_size: number, round: number, mask: bigint) {
+  if (M.byteLength !== word_size >> 2) {
+    throw new KitError(`ARC5-${word_size}/${round} block must be ${word_size >> 3} byte`)
   }
-  return C
+  const word_bit = BigInt(word_size)
+  const word_byte = word_size >> 3
+  const MV = U8.from(M).view(word_byte)
+  const SV = U8.from(S).view(word_byte)
+  // A = M[0] + S[0], B = M[1] + S[1]
+  let A = MV.get(0, true) + SV.get(0, true)
+  let B = MV.get(1, true) + SV.get(1, true)
+  A &= mask
+  B &= mask
+  for (let i = 1; i <= round; i++) {
+    // A = ((A ^ B) <<< B) + S[2 * i]
+    A = rotateL(word_bit, A ^ B, B, mask)
+    A += SV.get(i << 1, true)
+    A &= mask
+    // B = ((B ^ A) <<< A) + S[2 * i + 1]
+    B = rotateL(word_bit, B ^ A, A, mask)
+    B += SV.get((i << 1) + 1, true)
+    B &= mask
+  }
+  return U8.fromBI(B << word_bit | A, word_byte << 1, true)
 }
-function decrypt8(C: Uint8Array, S: Uint8Array, r: number) {
-  const M = C.slice(0)
-  for (let i = r; i > 0; i--) {
-    M[1] -= S[(i << 1) + 1]
-    M[1] = rotateR8(M[1], M[0] % 8) ^ M[0]
-    M[0] -= S[(i << 1)]
-    M[0] = rotateR8(M[0], M[1] % 8) ^ M[1]
+function _decrypt(C: Uint8Array, S: Uint8Array, word_size: number, round: number, mask: bigint) {
+  if (C.byteLength !== word_size >> 2) {
+    throw new KitError(`ARC5-${word_size}/${round} block must be ${word_size >> 3} byte`)
   }
-  M[0] -= S[0]
-  M[1] -= S[1]
-  return M
-}
-
-function setup16(K: Uint8Array, r: number) {
-  const P = 0xB7E1
-  const Q = 0x9E37
-  // Break the key into 16-bit words
-  const l = resizeBuffer(K, K.byteLength & 1 ? K.byteLength + 1 : K.byteLength)
-  const L = new Uint16Array(l.buffer)
-  const c = L.length
-  // Initialize key-independent pseudorandom S array
-  const t = (r + 1) << 1
-  const S = new Uint16Array(t)
-  S[0] = P
-  for (let i = 1; i < t; i++) {
-    S[i] = S[i - 1] + Q
+  const word_bit = BigInt(word_size)
+  const word_byte = word_size >> 3
+  const CV = U8.from(C).view(word_byte)
+  const SV = U8.from(S).view(word_byte)
+  // A = C[0], B = C[1]
+  let A = CV.get(0, true)
+  let B = CV.get(1, true)
+  for (let i = round; i > 0; i--) {
+    // B = ((B - S[2 * i + 1]) >>> A) ^ A
+    const S1 = SV.get((i << 1) + 1, true)
+    B = rotateR(word_bit, B - S1, A, mask)
+    B = B ^ A
+    B &= mask
+    // A = ((A - S[2 * i]) >>> B) ^ B
+    const S0 = SV.get(i << 1, true)
+    A = rotateR(word_bit, A - S0, B, mask)
+    A = A ^ B
+    A &= mask
   }
-  // The main key scheduling loop
-  let i = 0
-  let j = 0
-  let A = 0
-  let B = 0
-  const v = 3 * Math.max(c, t)
-  for (let k = 0; k < v; k++) {
-    A = S[i] = rotateL16(S[i] + A + B, 3)
-    B = L[j] = rotateL16(L[j] + A + B, A + B)
-    i = (i + 1) % t
-    j = (j + 1) % c
-  }
-  return new Uint8Array(S.buffer)
-}
-function encrypt16(M: Uint8Array, S: Uint8Array, r: number) {
-  const C = M.slice(0)
-  const C16 = new Uint16Array(C.buffer)
-  const S16 = new Uint16Array(S.buffer)
-  C16[0] += S16[0]
-  C16[1] += S16[1]
-  for (let i = 1; i <= r; i++) {
-    C16[0] = rotateL16(C16[0] ^ C16[1], C16[1] % 16)
-    C16[0] += S16[(i << 1)]
-    C16[1] = rotateL16(C16[1] ^ C16[0], C16[0] % 16)
-    C16[1] += S16[(i << 1) + 1]
-  }
-  return C
-}
-function decrypt16(C: Uint8Array, S: Uint8Array, r: number) {
-  const M = C.slice(0)
-  const M16 = new Uint16Array(M.buffer)
-  const S16 = new Uint16Array(S.buffer)
-  for (let i = r; i > 0; i--) {
-    M16[1] -= S16[(i << 1) + 1]
-    M16[1] = rotateR16(M16[1], M16[0] % 16) ^ M16[0]
-    M16[0] -= S16[i << 1]
-    M16[0] = rotateR16(M16[0], M16[1] % 16) ^ M16[1]
-  }
-  M16[0] -= S16[0]
-  M16[1] -= S16[1]
-  return M
-}
-
-function setup32(K: Uint8Array, r: number) {
-  const P = 0xB7E15163
-  const Q = 0x9E3779B9
-  // Break the key into 32-bit words
-  const l = resizeBuffer(K, ((K.byteLength + 3) >> 2 || 1) << 2)
-  const L = new Uint32Array(l.buffer)
-  const c = L.length
-  // Initialize key-independent pseudorandom S array
-  const t = (r + 1) << 1
-  const S = new Uint32Array(t)
-  S[0] = P
-  for (let i = 1; i < t; i++) {
-    S[i] = S[i - 1] + Q
-  }
-  // The main key scheduling loop
-  let i = 0
-  let j = 0
-  let A = 0
-  let B = 0
-  const v = 3 * Math.max(c, t)
-  for (let k = 0; k < v; k++) {
-    A = S[i] = rotateL32(S[i] + A + B, 3)
-    B = L[j] = rotateL32(L[j] + A + B, A + B)
-    i = (i + 1) % t
-    j = (j + 1) % c
-  }
-  return new Uint8Array(S.buffer)
-}
-function encrypt32(M: Uint8Array, S: Uint8Array, r: number) {
-  const C = M.slice(0)
-  const C32 = new Uint32Array(C.buffer)
-  const S32 = new Uint32Array(S.buffer)
-  C32[0] += S32[0]
-  C32[1] += S32[1]
-  for (let i = 1; i <= r; i++) {
-    C32[0] = rotateL32(C32[0] ^ C32[1], C32[1] % 32)
-    C32[0] += S32[(i << 1)]
-    C32[1] = rotateL32(C32[1] ^ C32[0], C32[0] % 32)
-    C32[1] += S32[(i << 1) + 1]
-  }
-  return C
-}
-function decrypt32(C: Uint8Array, S: Uint8Array, r: number) {
-  const M = C.slice(0)
-  const M32 = new Uint32Array(M.buffer)
-  const S32 = new Uint32Array(S.buffer)
-  for (let i = r; i > 0; i--) {
-    M32[1] -= S32[(i << 1) + 1]
-    M32[1] = rotateR32(M32[1], M32[0] % 32) ^ M32[0]
-    M32[0] -= S32[i << 1]
-    M32[0] = rotateR32(M32[0], M32[1] % 32) ^ M32[1]
-  }
-  M32[0] -= S32[0]
-  M32[1] -= S32[1]
-  return M
-}
-
-function setup64(K: Uint8Array, r: number) {
-  const P = 0xB7E151628AED2A6Bn
-  const Q = 0x9E3779B97F4A7C15n
-  // Break the key into 64-bit words
-  const l = resizeBuffer(K, ((K.byteLength + 7) >> 3 || 1) << 3)
-  const L = new BigUint64Array(l.buffer)
-  const c = L.length
-  // Initialize key-independent pseudorandom S array
-  const t = (r + 1) << 1
-  const S = new BigUint64Array(t)
-  S[0] = P
-  for (let i = 1; i < t; i++) {
-    S[i] = S[i - 1] + Q
-  }
-  // The main key scheduling loop
-  let i = 0
-  let j = 0
-  let A = 0n
-  let B = 0n
-  const v = 3 * Math.max(c, t)
-  for (let k = 0; k < v; k++) {
-    A = S[i] = rotateL64(S[i] + A + B, 3n)
-    B = L[j] = rotateL64(L[j] + A + B, A + B)
-    i = (i + 1) % t
-    j = (j + 1) % c
-  }
-  return new Uint8Array(S.buffer)
-}
-function encrypt64(M: Uint8Array, S: Uint8Array, r: number) {
-  const C = M.slice(0)
-  const C64 = new BigUint64Array(C.buffer)
-  const S64 = new BigUint64Array(S.buffer)
-  C64[0] += S64[0]
-  C64[1] += S64[1]
-  for (let i = 1; i <= r; i++) {
-    C64[0] = rotateL64(C64[0] ^ C64[1], C64[1] % 64n)
-    C64[0] += S64[(i << 1)]
-    C64[1] = rotateL64(C64[1] ^ C64[0], C64[0] % 64n)
-    C64[1] += S64[(i << 1) + 1]
-  }
-  return C
-}
-function decrypt64(C: Uint8Array, S: Uint8Array, r: number) {
-  const M = C.slice(0)
-  const M64 = new BigUint64Array(M.buffer)
-  const S64 = new BigUint64Array(S.buffer)
-  for (let i = r; i > 0; i--) {
-    M64[1] -= S64[(i << 1) + 1]
-    M64[1] = rotateR64(M64[1], M64[0]) ^ M64[0]
-    M64[0] -= S64[(i << 1)]
-    M64[0] = rotateR64(M64[0], M64[1]) ^ M64[1]
-  }
-  M64[0] -= S64[0]
-  M64[1] -= S64[1]
-  return M
-}
-
-function setup128(K: Uint8Array, r: number) {
-  const P = 0xB7E151628AED2A6ABF7158809CF4F3C7n
-  const Q = 0x9E3779B97F4A7C15F39CC0605CEDC835n
-  // Break the key into 64-bit words
-  const l = resizeBuffer(K, ((K.byteLength + 15) >> 4 || 1) << 4)
-  const c = l.byteLength >> 4
-  const L = Array.from<bigint>({ length: c }).fill(0n);
-  (function breakKey() {
-    const K64 = new BigUint64Array(l.buffer)
-    // littel-endian
-    for (let i = 0; i < c; i++) {
-      L[i] = K64[(i << 1)] | (K64[(i << 1) + 1] << 64n)
-    }
-  })()
-  const t = (r + 1) << 1
-  const S = Array.from<bigint>({ length: t }).fill(0n);
-  (function initS() {
-    S[0] = P
-    for (let i = 1; i < t; i++) {
-      S[i] = S[i - 1] + Q
-      S[i] &= 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFn
-    }
-  })()
-  let i = 0
-  let j = 0
-  let A = 0n
-  let B = 0n
-  const v = 3 * Math.max(c, t)
-  for (let k = 0; k < v; k++) {
-    A = S[i] = rotateL128(S[i] + A + B, 3n)
-    B = L[j] = rotateL128(L[j] + A + B, A + B)
-    i = (i + 1) % t
-    j = (j + 1) % c
-  }
-  const R = new BigUint64Array(t << 1)
-  S.forEach((v, i) => {
-    const h = v & 0xFFFFFFFFFFFFFFFFn
-    const l = v >> 64n
-    R[(i << 1)] = h
-    R[(i << 1) + 1] = l
-  })
-  return new Uint8Array(R.buffer)
-}
-function encrypt128(M: Uint8Array, S: Uint8Array, r: number) {
-  const C = M.slice(0)
-  const C64 = new BigUint64Array(C.buffer)
-  const S64 = new BigUint64Array(S.buffer)
-  let C0 = C64[0] | C64[1] << 64n
-  let C1 = C64[2] | C64[3] << 64n
-  C0 += S64[0] | S64[1] << 64n
-  C0 &= 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFn
-  C1 += S64[2] | S64[3] << 64n
-  C1 &= 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFn
-  for (let i = 1; i <= r; i++) {
-    C0 = rotateL128(C0 ^ C1, C1)
-    C0 += S64[(i << 2) + 0] | S64[(i << 2) + 1] << 64n
-    C0 &= 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFn
-    C1 = rotateL128(C1 ^ C0, C0)
-    C1 += S64[(i << 2) + 2] | S64[(i << 2) + 3] << 64n
-    C1 &= 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFn
-  }
-  C64[0] = C0 & 0xFFFFFFFFFFFFFFFFn
-  C64[1] = C0 >> 64n
-  C64[2] = C1 & 0xFFFFFFFFFFFFFFFFn
-  C64[3] = C1 >> 64n
-  return C
-}
-function decrypt128(C: Uint8Array, S: Uint8Array, r: number) {
-  const M = C.slice(0)
-  const M64 = new BigUint64Array(M.buffer)
-  const S64 = new BigUint64Array(S.buffer)
-  let M0 = M64[0] | M64[1] << 64n
-  let M1 = M64[2] | M64[3] << 64n
-  for (let i = r; i > 0; i--) {
-    M1 -= S64[(i << 2) + 2] | S64[(i << 2) + 3] << 64n
-    M1 &= 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFn
-    M1 = rotateR128(M1, M0) ^ M0
-    M0 -= S64[(i << 2) + 0] | S64[(i << 2) + 1] << 64n
-    M0 &= 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFn
-    M0 = rotateR128(M0, M1) ^ M1
-  }
-  M0 -= S64[0] | S64[1] << 64n
-  M0 &= 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFn
-  M1 -= S64[2] | S64[3] << 64n
-  M1 &= 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFn
-
-  M64[0] = M0 & 0xFFFFFFFFFFFFFFFFn
-  M64[1] = M0 >> 64n
-  M64[2] = M1 & 0xFFFFFFFFFFFFFFFFn
-  M64[3] = M1 >> 64n
-  return M
+  // A = A - S[0], B = B - S[1]
+  A = A - SV.get(0, true)
+  A &= mask
+  B = B - SV.get(1, true)
+  B &= mask
+  return U8.fromBI(B << word_bit | A, word_byte << 1, true)
 }
 
 // * ARC5 Algorithm
 
 function _arc5(K: Uint8Array, WORD_SIZE: 8 | 16 | 32 | 64 | 128, round: number) {
-  let S: Uint8Array
-  let _encrypt: (M: Uint8Array) => Uint8Array
-  let _decrypt: (C: Uint8Array) => Uint8Array
-  switch (WORD_SIZE) {
-    case 8:
-      S = setup8(K, round)
-      _encrypt = (M: Uint8Array) => encrypt8(M, S, round)
-      _decrypt = (C: Uint8Array) => decrypt8(C, S, round)
-      break
-    case 16:
-      S = setup16(K, round)
-      _encrypt = (M: Uint8Array) => encrypt16(M, S, round)
-      _decrypt = (C: Uint8Array) => decrypt16(C, S, round)
-      break
-    case 32:
-      S = setup32(K, round)
-      _encrypt = (M: Uint8Array) => encrypt32(M, S, round)
-      _decrypt = (C: Uint8Array) => decrypt32(C, S, round)
-      break
-    case 64:
-      S = setup64(K, round)
-      _encrypt = (M: Uint8Array) => encrypt64(M, S, round)
-      _decrypt = (C: Uint8Array) => decrypt64(C, S, round)
-      break
-    case 128:
-      S = setup128(K, round)
-      _encrypt = (M: Uint8Array) => encrypt128(M, S, round)
-      _decrypt = (C: Uint8Array) => decrypt128(C, S, round)
-      break
-    default:
-      throw new KitError('ARC5 word size must be 8, 16, 32, 64 or 128 bit')
-  }
-  return {
-    encrypt: (M: Uint8Array) => {
-      if (M.byteLength !== WORD_SIZE >> 2) {
-        throw new KitError(`ARC5-${WORD_SIZE}/${round} block must be ${WORD_SIZE >> 2} byte`)
-      }
-      return new U8(_encrypt(M))
-    },
-    decrypt: (C: Uint8Array) => {
-      if (C.byteLength !== WORD_SIZE >> 2) {
-        throw new KitError(`ARC5-${WORD_SIZE}/${round} block must be ${WORD_SIZE >> 2} byte`)
-      }
-      return new U8(_decrypt(C))
-    },
-  }
+  const mask = genBitMask(WORD_SIZE)
+  const S = _setup(K, WORD_SIZE, round, mask)
+  const encrypt = (M: Uint8Array) => _encrypt(M, S, WORD_SIZE, round, mask)
+  const decrypt = (C: Uint8Array) => _decrypt(C, S, WORD_SIZE, round, mask)
+  return { encrypt, decrypt }
 }
 
 /**
