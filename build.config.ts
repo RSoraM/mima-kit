8dbd0f2a1e98d6f2f74df61be7e9d0aae19ff9ee
RSoraM <soramephisto@gmail.com>
2025-02-10 16:18:50 +0800

    ci: 🎡 修改打包方式
    解决 issues/#1 和 pull/#8 的问题，该问题主要是 minify 导致打包后报错难看。修改为默认导入的包不进行 minify
    处理并生成sourceMap，保留tsc和minify的输出到单独的文件夹。

--- build.config.ts
+++ build.config.ts
@@ -1,16 +1,30 @@
 import { defineBuildConfig } from 'unbuild'
 
-export default defineBuildConfig({
-  failOnWarn: false,
-  entries: [
-    'src/index',
-  ],
-  declaration: true,
-  clean: true,
-  rollup: {
-    emitCJS: true,
-    esbuild: {
-      minify: true,
+export default defineBuildConfig([
+  {
+    name: 'default',
+    entries: ['src/index'],
+    outDir: 'dist',
+    clean: true,
+    failOnWarn: false,
+    sourcemap: true,
+    rollup: {
+      emitCJS: true,
+    },
+    declaration: true,
+  },
+  {
+    name: 'minified',
+    entries: ['src/index'],
+    outDir: 'dist/min',
+    clean: true,
+    failOnWarn: false,
+    sourcemap: true,
+    rollup: {
+      emitCJS: true,
+      esbuild: {
+        minify: true,
+      },
     },
   },
-})
+])
