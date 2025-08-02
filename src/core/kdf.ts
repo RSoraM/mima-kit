276236d92939199c0de88ca94ef8e63081c9df99
RSoraM <soramephisto@gmail.com>
2025-08-02 21:15:22 +0800

    perf: ⚡️ scrypt
    基于 @noble/hashes

--- src/core/kdf.ts
+++ src/core/kdf.ts
@@ -1,8 +1,7 @@
-import { salsa20Hash } from '../cipher/streamCipher/salsa20'
+import type { Hash, KeyHash } from './hash'
 import { hmac } from '../hash/hmac'
 import { sha256 } from '../hash/sha256'
-import type { Hash, KeyHash } from './hash'
-import { Counter, KitError, U8, joinBuffer } from './utils'
+import { Counter, joinBuffer, KitError, rotateL32, U8, u32 } from './utils'
 
 export interface KDF {
   /**
@@ -62,10 +61,8 @@ export function hkdf(k_hash: KeyHash, info = new Uint8Array(0)): KDF {
 
 /**
  * Password-Based Key Derivation Function 2 (PBKDF2), please combine `hmac` and `hash` externally to control the behavior of calling `hmac` inside the function.
- * Also, PBKDF2 does not use the `info` parameter, if provided, it will be ignored.
  *
  * PBKDF2 密码基础密钥派生函数 (PBKDF2), 请在外部组合 `hmac` 和 `hash` 函数, 以控制在函数内部调用 `hmac` 时的行为.
- * 同时, PBKDF2 不使用 `info` 参数, 如果提供 `info`, 将被忽略.
  */
 export function pbkdf2(k_hash: KeyHash, iterations = 1000): KDF {
   const d_byte = k_hash.DIGEST_SIZE
@@ -93,61 +90,180 @@ export function pbkdf2(k_hash: KeyHash, iterations = 1000): KDF {
   }
 }
 
-/** Scrypt Key Derivation Function, Block Mix */
-function scrypt_bm(r: number, B: Uint8Array) {
-  const Y = new U8(B.length)
-  let X = B.subarray(B.length - 64)
-  for (let i = 0; i < 2 * r; i++) {
-    const start = i << 6
-    const Bi = B.subarray(start, start + 64)
-    const T = X.map((_, j) => Bi[j] ^ X[j])
-    X = salsa20Hash(T, 8)
-    if (i & 1)
-      Y.set(X, ((i >> 1) << 6) + (r << 6))
-    else
-      Y.set(X, (i >> 1) << 6)
-  }
-  return Y
+interface ScryptConfig {
+  /**
+   * 开销因子 / Cost factor (default: 16384)
+   *
+   * 必须是 2 的幂
+   *
+   * Must be a power of 2
+   */
+  N?: number
+  /**
+   * 块数 / Block count (default: 8)
+   */
+  r?: number
+  /**
+   * 并行因子 / Parallelization factor (default: 1)
+   */
+  p?: number
+  /**
+   * 最大内存使用量 / Maximum memory usage
+   *
+   * 如果设置为 0，则不限制内存使用量
+   *
+   * If set to 0, there is no limit on memory usage
+   *
+   * (default: 0x40000400 bytes, 1GB + 1KB)
+   */
+  maxmem?: number
+  /**
+   * 密钥派生函数 / Key Derivation Function
+   *
+   * scrypt 标准使用了 `PBKDF2-HMAC-SHA256` 作为 KDF。
+   * 该参数允许用户指定其他 KDF，改变 scrypt 的内部行为。
+   *
+   * 注意: 这不是 `scrypt` 的标准用法且缺乏相关的安全分析。
+   *
+   * The scrypt standard uses `PBKDF2-HMAC-SHA256` as the KDF.
+   * This parameter allows users to specify a different KDF, changing the internal behavior of scrypt.
+   *
+   * Note: This is not the standard usage of `scrypt` and lacks relevant security analysis.
+   *
+   * (default: pbkdf2(hmac(sha256), 1))
+   */
+  kdf?: KDF
 }
-/** Scrypt Key Derivation Function, ROM Mix */
-function scrypt_rm(r: number, B: Uint8Array, N: number) {
-  const V = new Uint8Array(N * r << 7)
-  let X = U8.from(B)
-  for (let i = 0; i < N; i++) {
-    V.set(X, i * r << 7)
-    X = scrypt_bm(r, X)
-  }
 
-  const Nn = BigInt(N)
-  const start = X.length - 64
-  for (let i = 0; i < N; i++) {
-    const j = Number(X.subarray(start).toBI(true) % Nn)
-    const Vi = V.subarray(j * r << 7, (j + 1) * r << 7)
-    X.forEach((_, k) => X[k] ^= Vi[k])
-    X = scrypt_bm(r, X)
-  }
+/**
+ * Scrypt Key Derivation Function
+ *
+ * Scrypt 密钥派生函数
+ *
+ * Based on https://github.com/paulmillr/noble-hashes
+ */
+export function scrypt(config?: ScryptConfig): KDF {
+  const {
+    N = 16384,
+    r = 8,
+    p = 1,
+    maxmem = 0x40000400,
+    kdf = pbkdf2(hmac(sha256), 1),
+  } = config ?? {}
 
-  return U8.from(X)
-}
-export function scrypt(N = 16384, r = 8, p = 1): KDF {
-  if (N === 0 || (N & (N - 1)) !== 0)
+  const BLOCK_SIZE = r << 7
+  const BLOCK_SIZE_32 = r << 5
+  const MAX_p = (0x1FFFFFFFE0 / BLOCK_SIZE) >>> 0
+  const MEM_COST = BLOCK_SIZE * (N + p)
+  const N_1 = N - 1
+
+  if (N === 0 || (N & N_1) !== 0)
     throw new KitError(`N must be a power of 2`)
-  const MAX_N = (0xFFFFFF / r) >>> 0
-  const MAX_r = (0xFFFFFF / p) >>> 0
-  if (N > MAX_N)
-    throw new KitError(`N too large, N <= ${MAX_N}`)
-  if (r > MAX_r)
-    throw new KitError(`r too large, r <= ${MAX_r}`)
+  if (p < 1 || p > MAX_p)
+    throw new KitError(`p must be in range [1, ${MAX_p}]`)
+  if (MEM_COST > maxmem)
+    throw new KitError(`Memory cost exceeds maxmem: ${MEM_COST} > ${maxmem}`)
 
-  const kh = hmac(sha256)
+  // 内联 rotateL32 (神秘的黑魔法)
+  const rotl = rotateL32
+
+  function fast_xor_salsa_hash(
+    prev: Uint32Array,
+    pi: number,
+    input: Uint32Array,
+    ii: number,
+    output: Uint32Array,
+    oi: number,
+  ) {
+    const y00 = prev[pi++] ^ input[ii++]; const y01 = prev[pi++] ^ input[ii++]
+    const y02 = prev[pi++] ^ input[ii++]; const y03 = prev[pi++] ^ input[ii++]
+    const y04 = prev[pi++] ^ input[ii++]; const y05 = prev[pi++] ^ input[ii++]
+    const y06 = prev[pi++] ^ input[ii++]; const y07 = prev[pi++] ^ input[ii++]
+    const y08 = prev[pi++] ^ input[ii++]; const y09 = prev[pi++] ^ input[ii++]
+    const y10 = prev[pi++] ^ input[ii++]; const y11 = prev[pi++] ^ input[ii++]
+    const y12 = prev[pi++] ^ input[ii++]; const y13 = prev[pi++] ^ input[ii++]
+    const y14 = prev[pi++] ^ input[ii++]; const y15 = prev[pi++] ^ input[ii++]
+    // Save state to temporary variables (salsa)
+    let x00 = y00; let x01 = y01; let x02 = y02; let x03 = y03
+    let x04 = y04; let x05 = y05; let x06 = y06; let x07 = y07
+    let x08 = y08; let x09 = y09; let x10 = y10; let x11 = y11
+    let x12 = y12; let x13 = y13; let x14 = y14; let x15 = y15
+    // Main loop (salsa)
+    for (let i = 0; i < 8; i += 2) {
+      x04 ^= rotl(x00 + x12 | 0,  7); x08 ^= rotl(x04 + x00 | 0,  9)
+      x12 ^= rotl(x08 + x04 | 0, 13); x00 ^= rotl(x12 + x08 | 0, 18)
+      x09 ^= rotl(x05 + x01 | 0,  7); x13 ^= rotl(x09 + x05 | 0,  9)
+      x01 ^= rotl(x13 + x09 | 0, 13); x05 ^= rotl(x01 + x13 | 0, 18)
+      x14 ^= rotl(x10 + x06 | 0,  7); x02 ^= rotl(x14 + x10 | 0,  9)
+      x06 ^= rotl(x02 + x14 | 0, 13); x10 ^= rotl(x06 + x02 | 0, 18)
+      x03 ^= rotl(x15 + x11 | 0,  7); x07 ^= rotl(x03 + x15 | 0,  9)
+      x11 ^= rotl(x07 + x03 | 0, 13); x15 ^= rotl(x11 + x07 | 0, 18)
+      x01 ^= rotl(x00 + x03 | 0,  7); x02 ^= rotl(x01 + x00 | 0,  9)
+      x03 ^= rotl(x02 + x01 | 0, 13); x00 ^= rotl(x03 + x02 | 0, 18)
+      x06 ^= rotl(x05 + x04 | 0,  7); x07 ^= rotl(x06 + x05 | 0,  9)
+      x04 ^= rotl(x07 + x06 | 0, 13); x05 ^= rotl(x04 + x07 | 0, 18)
+      x11 ^= rotl(x10 + x09 | 0,  7); x08 ^= rotl(x11 + x10 | 0,  9)
+      x09 ^= rotl(x08 + x11 | 0, 13); x10 ^= rotl(x09 + x08 | 0, 18)
+      x12 ^= rotl(x15 + x14 | 0,  7); x13 ^= rotl(x12 + x15 | 0,  9)
+      x14 ^= rotl(x13 + x12 | 0, 13); x15 ^= rotl(x14 + x13 | 0, 18)
+    }
+    // Write output (salsa)
+    output[oi++] = (y00 + x00) | 0; output[oi++] = (y01 + x01) | 0
+    output[oi++] = (y02 + x02) | 0; output[oi++] = (y03 + x03) | 0
+    output[oi++] = (y04 + x04) | 0; output[oi++] = (y05 + x05) | 0
+    output[oi++] = (y06 + x06) | 0; output[oi++] = (y07 + x07) | 0
+    output[oi++] = (y08 + x08) | 0; output[oi++] = (y09 + x09) | 0
+    output[oi++] = (y10 + x10) | 0; output[oi++] = (y11 + x11) | 0
+    output[oi++] = (y12 + x12) | 0; output[oi++] = (y13 + x13) | 0
+    output[oi++] = (y14 + x14) | 0; output[oi++] = (y15 + x15) | 0
+  }
+  function block_mix(
+    input: Uint32Array,
+    input_index: number,
+    output: Uint32Array,
+    output_index: number,
+    r: number,
+  ) {
+    let head = output_index
+    let tail = output_index + (r << 4)
+    const t = ((r << 1) - 1) << 4
+    for (let i = 0; i < 16; i++) output[tail + i] = input[input_index + i + t]
+    for (let i = 0; i < r; i++) {
+      fast_xor_salsa_hash(output, tail, input, input_index, output, head)
+      if (i > 0) { tail += 16 }
+      input_index += 16
+      fast_xor_salsa_hash(output, head, input, input_index, output, tail)
+      head += 16
+      input_index += 16
+    }
+  }
+
   return (k_byte: number, ikm: Uint8Array, salt = new Uint8Array(0)) => {
-    const B = pbkdf2(kh, 1)(r * p << 10, ikm, salt.slice())
-    const T = []
-    for (let i = 0; i < p; i++) {
-      const Bi = B.subarray(i * r << 7, (i + 1) * r << 7)
-      T.push(scrypt_rm(r, Bi, N))
+    const B = kdf(BLOCK_SIZE * p, ikm, salt)
+    const B32 = u32(B)
+
+    const V32 = u32(new Uint8Array(BLOCK_SIZE * N))
+    const tmp = u32(new Uint8Array(BLOCK_SIZE))
+
+    for (let pi = 0; pi < p; pi++) {
+      const PI = BLOCK_SIZE_32 * pi
+      V32.set(B32.subarray(PI, PI + BLOCK_SIZE_32), 0)
+      let pos = 0
+      for (let i = 0; i < N_1; i++) {
+        block_mix(V32, pos, V32, pos + BLOCK_SIZE_32, r)
+        pos += BLOCK_SIZE_32
+      }
+      block_mix(V32, N_1 * BLOCK_SIZE_32, B32, PI, r)
+
+      for (let i = 0; i < N; i++) {
+        const j = B32[PI + BLOCK_SIZE_32 - 16] % N
+        for (let k = 0; k < BLOCK_SIZE_32; k++) {
+          tmp[k] = B32[PI + k] ^ V32[j * BLOCK_SIZE_32 + k]
+        }
+        block_mix(tmp, 0, B32, PI, r)
+      }
     }
 
-    return pbkdf2(kh, 1)(k_byte, ikm, joinBuffer(...T))
+    return kdf(k_byte, ikm, B)
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,106 @@
-// * Utils
-export { U8, joinBuffer } from './core/utils'
-export type { RandomPrimeGenerator } from './core/prime'
-export { genPrime, isProbablePrime } from './core/prime'
+// * Block Cipher
+export { aes } from './cipher/blockCipher/aes'
+export { arc5 } from './cipher/blockCipher/arc5'
+export { aria } from './cipher/blockCipher/aria'
+export { blowfish } from './cipher/blockCipher/blowfish'
+export { camellia } from './cipher/blockCipher/camellia'
+export { des, t_des } from './cipher/blockCipher/des'
+export { sm4 } from './cipher/blockCipher/sm4'
+export type { XXTEAConfig } from './cipher/blockCipher/tea'
+export { tea, xtea, xxtea } from './cipher/blockCipher/tea'
+export { twofish } from './cipher/blockCipher/twofish'
+
+// * ECC
+export type { ECKeyPair, ECPrivateKey, ECPublicKey } from './cipher/pkcs/ecc'
+export type { ECDSASignature, ECIESCiphertext, IVBlockCipher } from './cipher/pkcs/ecc'
+export type { FpECCrypto } from './cipher/pkcs/ecc'
+export { FpECC } from './cipher/pkcs/ecc'
+export { pkcs1_es_1_5, pkcs1_es_oaep } from './cipher/pkcs/pkcs1'
+export { pkcs1_ssa_1_5, pkcs1_ssa_pss } from './cipher/pkcs/pkcs1'
+
+// * MGF
+export type { MGF } from './cipher/pkcs/pkcs1'
+export { mgf1 } from './cipher/pkcs/pkcs1'
+
+// * RSA
+export type { RSAPrivateKey, RSAPublicKey } from './cipher/pkcs/rsa'
+export { rsa } from './cipher/pkcs/rsa'
+
+// * SM2
+export type { FpSM2Crypto } from './cipher/pkcs/sm2'
+export type { SM2DSASignature } from './cipher/pkcs/sm2'
+export { sm2 } from './cipher/pkcs/sm2'
+
+// * X25519
+export type { X25519KeyPair, X25519PrivateKey, X25519PublicKey } from './cipher/pkcs/x25519_448'
+export type { X448KeyPair, X448PrivateKey, X448PublicKey } from './cipher/pkcs/x25519_448'
+export type { X448, X25519 } from './cipher/pkcs/x25519_448'
+export { x448, x25519 } from './cipher/pkcs/x25519_448'
+
+// * Stream Cipher
+export { arc4 } from './cipher/streamCipher/arc4'
+export { rabbit } from './cipher/streamCipher/rabbit'
+export { salsa20 } from './cipher/streamCipher/salsa20'
+export type { ZUCParams } from './cipher/streamCipher/zuc'
+export { eea3, eia3, zuc } from './cipher/streamCipher/zuc'
+
+// * Cipher Utils
+export type { Cipher, IVCipher } from './core/cipher'
+export type { BlockCipherInfo, IVCipherInfo, StreamCipherInfo } from './core/cipher'
+export type { BlockCipher, IVStreamCipher, StreamCipher } from './core/cipher'
+export { createCipher } from './core/cipher'
+
+// * Block Cipher Modes
+export { cbc, cfb, ctr, ecb, gcm, ofb, pcbc } from './core/cipher'
+
+// * Block Cipher Padding
+export { ISO7816_PAD, NO_PAD, PKCS7_PAD, X923_PAD, ZERO_PAD } from './core/cipher'
 
 // * Codec
 export type { Codec } from './core/codec'
-export { UTF8, HEX, B32, B64, B64URL, CSV } from './core/codec'
+export { B32, B64, B64URL, CSV, HEX, UTF8 } from './core/codec'
+export type { FpECPoint } from './core/ecParams'
+export type { FpMECParams, FpWECParams } from './core/ecParams'
+export { sm2p256v1 } from './core/ecParams'
+
+// TODO 实现 爱德华曲线 后再开放
+// export { ed25519, ed448 } from './core/ecParams'
+// export { secp112r1, secp112r2 } from './core/ecParams'
+// export { secp128r1, secp128r2 } from './core/ecParams'
+// export { secp160k1, secp160r1, secp160r2 } from './core/ecParams'
+export { secp192k1, secp192r1 } from './core/ecParams'
+export { secp224k1, secp224r1 } from './core/ecParams'
+export { secp256k1, secp256r1 } from './core/ecParams'
+export { secp384r1, secp521r1 } from './core/ecParams'
+export { prime192v1, prime256v1 } from './core/ecParams'
+export { p192, p224, p256, p384, p521 } from './core/ecParams'
+export { w448, w25519 } from './core/ecParams'
+export { curve448, curve25519 } from './core/ecParams'
+export { bp192r1, bp224r1, bp256r1, bp320r1, bp384r1, bp512r1 } from './core/ecParams'
 
 // * Hash Utils
-export type { Digest, HashDescription, Hash } from './core/hash'
-export type { KeyDigest, KeyHashDescription, KeyHash } from './core/hash'
-export type { TupleDigest, TupleHashDescription, TupleHash } from './core/hash'
+export type { Digest, Hash, HashDescription } from './core/hash'
+export type { KeyDigest, KeyHash, KeyHashDescription } from './core/hash'
+export type { TupleDigest, TupleHash, TupleHashDescription } from './core/hash'
 export { createHash, createTupleHash } from './core/hash'
+
+// * KDF
+export type { KDF } from './core/kdf'
+export { hkdf, pbkdf2, scrypt, x963kdf } from './core/kdf'
+
+// * Utils
+export type { RandomPrimeGenerator } from './core/prime'
+export { genPrime, isProbablePrime } from './core/prime'
+export { joinBuffer, U8 } from './core/utils'
+
+// * HMAC
+export { hmac } from './hash/hmac'
+
+// * KangarooTwelve
+export { kt128, kt256 } from './hash/kangaroo12'
+
+// * SHA-3 Keccak
+export { keccak_p_1600, sponge_1600 } from './hash/keccak1600'
 
 // * MD5
 export { md5 } from './hash/md5'
@@ -19,12 +108,7 @@ export { md5 } from './hash/md5'
 // * SHA-1
 export { sha1 } from './hash/sha1'
 
-// * SHA-2
-export { sha224, sha256 } from './hash/sha256'
-export { sha384, sha512, sha512t } from './hash/sha512'
-
 // * SHA-3
-export { keccak_p_1600, sponge_1600 } from './hash/keccak1600'
 export { sha3_224, sha3_256 } from './hash/sha3'
 export { sha3_384, sha3_512 } from './hash/sha3'
 
@@ -46,96 +130,15 @@ export { tuplehash256, tuplehash256XOF } from './hash/sha3Derived'
 export { parallelhash128, parallelhash128XOF } from './hash/sha3Derived'
 export { parallelhash256, parallelhash256XOF } from './hash/sha3Derived'
 
-// * TurboSHAKE
-export { turboshake128, turboshake256 } from './hash/turboSHAKE'
-
-// * KangarooTwelve
-export { kt128, kt256 } from './hash/kangaroo12'
+// * SHA-2
+export { sha224, sha256 } from './hash/sha256'
+export { sha384, sha512, sha512t } from './hash/sha512'
 
 // * SM3
 export { sm3 } from './hash/sm3'
 
-// * HMAC
-export { hmac } from './hash/hmac'
-
 // * TOTP
 export { totp } from './hash/totp'
 
-// * Cipher Utils
-export type { Cipher, IVCipher } from './core/cipher'
-export type { BlockCipherInfo, StreamCipherInfo, IVCipherInfo } from './core/cipher'
-export type { BlockCipher, StreamCipher, IVStreamCipher } from './core/cipher'
-export { createCipher } from './core/cipher'
-
-// * Block Cipher
-export { sm4 } from './cipher/blockCipher/sm4'
-export { aes } from './cipher/blockCipher/aes'
-export { aria } from './cipher/blockCipher/aria'
-export { camellia } from './cipher/blockCipher/camellia'
-export { des, t_des } from './cipher/blockCipher/des'
-export { arc5 } from './cipher/blockCipher/arc5'
-export { blowfish } from './cipher/blockCipher/blowfish'
-export { twofish } from './cipher/blockCipher/twofish'
-export type { XXTEAConfig } from './cipher/blockCipher/tea'
-export { tea, xtea, xxtea } from './cipher/blockCipher/tea'
-
-// * Block Cipher Modes
-export { ecb, cbc, pcbc, cfb, ofb, ctr, gcm } from './core/cipher'
-
-// * Block Cipher Padding
-export { PKCS7_PAD, ZERO_PAD, X923_PAD, ISO7816_PAD, NO_PAD } from './core/cipher'
-
-// * Stream Cipher
-export type { ZUCParams } from './cipher/streamCipher/zuc'
-export { eea3, eia3, zuc } from './cipher/streamCipher/zuc'
-export { arc4 } from './cipher/streamCipher/arc4'
-export { salsa20 } from './cipher/streamCipher/salsa20'
-export { rabbit } from './cipher/streamCipher/rabbit'
-
-// * RSA
-export type { RSAPublicKey, RSAPrivateKey } from './cipher/pkcs/rsa'
-export { rsa } from './cipher/pkcs/rsa'
-export { pkcs1_es_1_5, pkcs1_es_oaep } from './cipher/pkcs/pkcs1'
-export { pkcs1_ssa_1_5, pkcs1_ssa_pss } from './cipher/pkcs/pkcs1'
-
-// * MGF
-export type { MGF } from './cipher/pkcs/pkcs1'
-export { mgf1 } from './cipher/pkcs/pkcs1'
-
-// * KDF
-export type { KDF } from './core/kdf'
-export { x963kdf, hkdf, pbkdf2 } from './core/kdf'
-
-// * ECC
-export type { ECPrivateKey, ECPublicKey, ECKeyPair } from './cipher/pkcs/ecc'
-export type { IVBlockCipher, ECIESCiphertext, ECDSASignature } from './cipher/pkcs/ecc'
-export type { FpECCrypto } from './cipher/pkcs/ecc'
-export { FpECC } from './cipher/pkcs/ecc'
-export type { FpECPoint } from './core/ecParams'
-export type { FpWECParams, FpMECParams } from './core/ecParams'
-export { sm2p256v1 } from './core/ecParams'
-// export { secp112r1, secp112r2 } from './core/ecParams'
-// export { secp128r1, secp128r2 } from './core/ecParams'
-// export { secp160k1, secp160r1, secp160r2 } from './core/ecParams'
-export { secp192k1, secp192r1 } from './core/ecParams'
-export { secp224k1, secp224r1 } from './core/ecParams'
-export { secp256k1, secp256r1 } from './core/ecParams'
-export { secp384r1, secp521r1 } from './core/ecParams'
-export { prime192v1, prime256v1 } from './core/ecParams'
-export { p192, p224, p256, p384, p521 } from './core/ecParams'
-export { w25519, w448 } from './core/ecParams'
-// TODO 实现 爱德华曲线 后再开放
-// export { ed25519, ed448 } from './core/ecParams'
-export { curve25519, curve448 } from './core/ecParams'
-export { bp192r1, bp224r1, bp256r1, bp320r1, bp384r1, bp512r1 } from './core/ecParams'
-
-// * SM2
-export type { FpSM2Crypto } from './cipher/pkcs/sm2'
-export type { SM2DSASignature } from './cipher/pkcs/sm2'
-export { sm2 } from './cipher/pkcs/sm2'
-
-// * X25519
-export type { X25519PrivateKey, X25519PublicKey, X25519KeyPair } from './cipher/pkcs/x25519_448'
-export type { X448PrivateKey, X448PublicKey, X448KeyPair } from './cipher/pkcs/x25519_448'
-export type { X25519, X448 } from './cipher/pkcs/x25519_448'
-export { x25519, x448 } from './cipher/pkcs/x25519_448'
+// * TurboSHAKE
+export { turboshake128, turboshake256 } from './hash/turboSHAKE'
